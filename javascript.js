657f439e40f4c403bc33463faa17d78de163a9ba
SHIVANG HARMILAPI <shivangkishanharmilapi@gmail.com>
2019-08-22 10:45:53 +0530

    Added autopopulate property

--- javascript.js
+++ javascript.js
@@ -6,13 +6,13 @@ function func(ele)
    for(i=0;i<a.length;i++)
       a[i].style.display="none";
 //var clicked=document.getElementById(ele);
-  if(ele==="home")
+  if(ele==="todo")
       clicked.style.display="block"
-  else if(ele==="news")
+  else if(ele==="user")
       clicked.style.display="block"
-  else if(ele==="contact")
+  else if(ele==="display")
       clicked.style.display="block"
-  else if(ele==="about")
+  else if(ele==="logout")
       clicked.style.display="block"
 }
 
@@ -37,41 +37,141 @@ function hidePrompt()
 
 
 
-function filterFunction()
-{
-  let input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  console.log(filter);
-  a = document.getElementsByClassName("itemlist");
-  for (i = 0; i < a.length; i++)
-  {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (!txtValue.startsWith(filter))
-    {
-      a[i].style.display = "none";
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+
+
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+
+
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
     }
-    else
-    {
-      a[i].style.display = "block";
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
     }
   }
 }
-
-function populateTodoList()
-{const fs = require('fs');
-const json_data = require('../todo.JSON');
-
-fs.readFile(json_data, 'utf8', function (err, data) {
-  try {
-    data = JSON.parse(data)
-    for (let i in data){
-    console.log('Name:',data[i].name)
-    }
-  } catch (e) {
-    // Catch error in case file doesn't exist or isn't valid JSON
-  }
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
 });
+}
 
+var countries = [];
+function deleteRow(row) {
+  var i = row.parentNode.parentNode.rowIndex;
+  document.getElementById('POITable').deleteRow(i);
+}
+
+function updateDetail(cell)
+{
+  var person = prompt("Please enter your todo", "");
+  if (person != null) {
+    cell.parentNode.parentNode.cells[0].innerHTML = person ;
+  }
 
 }
+
+
+var trid=1;
+var tdid=4;
+function insRow() {
+  var x = document.getElementById('POITable');
+  var new_row = x.rows[0].cloneNode(true);
+  new_row.setAttribute("id","tr"+trid);
+  new_row.style.display="block";
+  var inp1 = document.getElementById('input').value;
+  new_row.cells[0].innerHTML=trid++;
+  new_row.cells[0].setAttribute("id","td"+tdid++);
+  new_row.cells[1].setAttribute("id","td"+tdid++);
+  new_row.cells[2].setAttribute("id","td"+tdid++);
+  
+  new_row.cells[0].innerHTML = inp1;
+  x.appendChild(new_row);
+  countries.push(inp1);
+}
+
+
+
